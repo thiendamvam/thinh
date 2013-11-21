be297ecc3b8792add31b10caeeace47b5a835be2
thien.dam@audiencemedia.com <thien.dam@audiencemedia.com>
2013-11-21 20:05:40 +0700

    aadfdsfsd

---
+++ DogReview/src/com/gso/dogreview/fragment/FragmentView.java
@@ -0,0 +1,17 @@
+package com.gso.dogreview.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentView extends DialogFragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		In
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}

e29bad04a47cdf6e2f664433f639a910c2cb219c
Thien Dam <thienuit@gmail.com>
2013-12-16 23:33:40 +0700

    Add file

---
+++ DogReview/src/com/gso/dogreview/fragment/InfoContentFragment.java
@@ -0,0 +1,26 @@
+package com.gso.dogreview.fragment;
+
+import android.location.Address;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoContentFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View v = inflater.from(container.getContext()).inflate(R.layout., container, false);
+		
+	}
+}
